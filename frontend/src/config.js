dd2ef8510360f57231894bbe21f2359192137eef
Yuichiro Kondo <kondo_u16@yahoo.co.jp>
2024-03-13 23:14:18 +0900

    Remove contract json files from frontend

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,14 +1,14 @@
-var DeedRepository = require('./contracts/DeedRepository')
-var AuctionRepository = require('./contracts/AuctionRepository')
+var DeedRepository = require("./contracts/DeedRepository");
+var AuctionRepository = require("./contracts/AuctionRepository");
 
 module.exports = {
-    JSONRPC_ENDPOINT: 'http://52.59.238.144:8545',
-    JSONRPC_WS_ENDPOINT: 'ws://52.59.238.144:8546', //'ws://52.59.238.144:8546',
-    BZZ_ENDPOINT: 'http://52.59.238.144:8500',
-    SHH_ENDPOINT: 'ws://52.59.238.144:8546',
+    JSONRPC_ENDPOINT: "http://localhost:7545",
+    JSONRPC_WS_ENDPOINT: "ws://localhost:7545",
+    BZZ_ENDPOINT: "http://localhost:8500",
+    SHH_ENDPOINT: "ws://localhost:7545",
 
-    DEEDREPOSITORY_ADDRESS: '0xfc35c45cd57661197d0bb19399c9d3ede1c50dcc',
-    AUCTIONREPOSITORY_ADDRESS: '0xefbebbf64a570f7b94f168430f45ecbb87546f06',
+    DEEDREPOSITORY_ADDRESS: "0xC3daB6B7c84b0342607D5904b7214b7F1E3DAad6",
+    AUCTIONREPOSITORY_ADDRESS: "0x29a412B146aAe61A0256707065cb94Bd4fcC6c8b",
 
     DEEDREPOSITORY_ABI: DeedRepository.abi,
     AUCTIONREPOSITORY_ABI: AuctionRepository.abi,
@@ -16,8 +16,8 @@ module.exports = {
     GAS_AMOUNT: 500000,
 
     //whisper settings
-    WHISPER_SHARED_KEY: '0x8bda3abeb454847b515fa9b404cede50b1cc63cfdeddd4999d074284b4c21e15'
-
-}
+    WHISPER_SHARED_KEY:
+        "0x8bda3abeb454847b515fa9b404cede50b1cc63cfdeddd4999d074284b4c21e15",
+};
 
 // web3.eth.sendTransaction({from: web3.eth.accounts[0], to: "0x6f0023D1CFe5A7A56F96e61E0169B775Ac97f90E" , value: web3.utils.toWei(1, 'ether'), gasLimit: 21000, gasPrice: 20000000000})
