6847deb26cd5f49f4d48351fa014c007a3a24251
Yuichiro Kondo <kondo_u16@yahoo.co.jp>
2024-03-19 10:22:25 +0900

    Add settings for Sepolia

--- backend/truffle.js
+++ backend/truffle.js
@@ -1,3 +1,6 @@
+require("dotenv").config();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
 module.exports = {
   networks: {
     dev: {
@@ -10,6 +13,18 @@ module.exports = {
       host: "localhost",
       port: 7545,
       gas: 6721975,
+    },
+    sepolia: {
+      provider: () =>
+        new HDWalletProvider(
+          process.env["SEPOLIA_PRIVATE_KEY"],
+          `https://eth-sepolia.g.alchemy.com/v2/${process.env["ALCHEMY_API_KEY"]}`
+        ),
+      network_id: 11155111,
+      gas: 5500000,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
     },
   },
 };
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,13 +2,16 @@ var DeedRepository = require("./contracts/DeedRepository");
 var AuctionRepository = require("./contracts/AuctionRepository");
 
 module.exports = {
-    JSONRPC_ENDPOINT: "http://localhost:7545",
-    JSONRPC_WS_ENDPOINT: "ws://localhost:7545",
     BZZ_ENDPOINT: "http://localhost:8500",
-    SHH_ENDPOINT: "ws://localhost:7545",
+    SHH_ENDPOINT: "ws://localhost:12345",
 
-    DEEDREPOSITORY_ADDRESS: "0xC3daB6B7c84b0342607D5904b7214b7F1E3DAad6",
-    AUCTIONREPOSITORY_ADDRESS: "0x29a412B146aAe61A0256707065cb94Bd4fcC6c8b",
+    // For local testing as Ganache
+    // DEEDREPOSITORY_ADDRESS: "0xC3daB6B7c84b0342607D5904b7214b7F1E3DAad6",
+    // AUCTIONREPOSITORY_ADDRESS: "0x29a412B146aAe61A0256707065cb94Bd4fcC6c8b",
+
+    // For testing as Sepolia
+    DEEDREPOSITORY_ADDRESS: "0x812Ce7195D9fCafcF644e545951119523258B81a",
+    AUCTIONREPOSITORY_ADDRESS: "0x330a328470741193641b3B77b64FDd6a5249aB64",
 
     DEEDREPOSITORY_ABI: DeedRepository.abi,
     AUCTIONREPOSITORY_ABI: AuctionRepository.abi,
@@ -19,5 +22,3 @@ module.exports = {
     WHISPER_SHARED_KEY:
         "0x8bda3abeb454847b515fa9b404cede50b1cc63cfdeddd4999d074284b4c21e15",
 };
-
-// web3.eth.sendTransaction({from: web3.eth.accounts[0], to: "0x6f0023D1CFe5A7A56F96e61E0169B775Ac97f90E" , value: web3.utils.toWei(1, 'ether'), gasLimit: 21000, gasPrice: 20000000000})
